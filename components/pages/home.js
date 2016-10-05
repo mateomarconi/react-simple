02ad9b79c4fb1c236ac93572fcd1b384034d2403
lzielinski03 <lzielinski03@gmail.com>
2016-10-05 01:21:16 -0300

    layout layer

--- components/pages/home.js
+++ components/pages/home.js
@@ -123,6 +123,11 @@ const HomePage = React.createClass({
 						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+						<Box>
+							<div className="test"></div>
+							<div className="test"></div>
+						</Box>
 					</DisplayLayer>
 				</Box>
 
